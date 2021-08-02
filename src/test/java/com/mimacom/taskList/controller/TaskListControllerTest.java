4856bfa67aabfad5a388125a9e345d6c2bc6b49b
Paula Bonastre <pbonastre@gmail.com>
2021-08-02 04:39:17 +0200

    Add some tests

--- src/test/java/com/mimacom/taskList/controller/TaskListControllerTest.java
+++ src/test/java/com/mimacom/taskList/controller/TaskListControllerTest.java
@@ -10,7 +10,11 @@ import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 import com.mimacom.taskList.service.TaskService;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.text.ParseException;
 import java.util.ArrayList;
@@ -24,7 +28,7 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(MockitoExtension.class)
-public class TaskListControllerTest{
+public class TaskListControllerTest {
 
     @Mock
     private TaskService taskService;
@@ -33,53 +37,67 @@ public class TaskListControllerTest{
 
     private List<Task> taskList;
     private Task task1;
+    private MockMvc mockMvc;
 
     @BeforeEach
-    public void init(){
+    public void init() {
         MockitoAnnotations.initMocks(this);
         this.taskListController = new TaskListController(taskService);
+        mockMvc = MockMvcBuilders.standaloneSetup(taskListController).build();
 
         task1 = new Task("title1", "description1");
         var task2 = new Task("title2", "description2");
         var task3 = new Task("title3", "description3");
-        taskList = Arrays.asList(task1,task2,task3);
+        taskList = Arrays.asList(task1, task2, task3);
     }
 
     @Test
-    public void createTask(){
+    public void createTask() {
         lenient().doReturn(task1).when(taskService).createTask(any());
         ResponseEntity<Task> response = taskListController.createTask(task1);
         assertNotNull(response);
-        assertEquals("Title1",response.getBody().getTitle());
+        assertEquals("Title1", response.getBody().getTitle());
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
     }
 
     @Test
-    public void getAllTasks(){
+    public void getAllTasks() {
         lenient().doReturn(taskList).when(taskService).getAllTasks();
         ResponseEntity<List<Task>> response = taskListController.getAllTasks();
         assertNotNull(response);
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
-        assertEquals(response.getBody().size(),3);
+        assertEquals(response.getBody().size(), 3);
     }
 
-     @Test
-    public void updateTaskById(){
+    @Test
+    public void updateTaskById() {
         lenient().doReturn(task1).when(taskService).updateTask(any());
-        ResponseEntity<Task> response = taskListController.updateTaskById(Long.getLong("1"),task1);
+        ResponseEntity<Task> response = taskListController.updateTaskById(Long.getLong("1"), task1);
         assertNotNull(response);
-        assertEquals("title1",response.getBody().getTitle());
+        assertEquals("title1", response.getBody().getTitle());
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+   }
 
-
-    }
     @Test
     public void setTaskAsFinish() throws ParseException {
-        lenient().doNothing().when(taskService).setTaskAsFinish(any(),any());
-        ResponseEntity<String> response =  taskListController.setTaskAsFinish(Long.valueOf(1),"20210725");
+        lenient().doNothing().when(taskService).setTaskAsFinish(any(), any());
+        ResponseEntity<String> response = taskListController.setTaskAsFinish(Long.valueOf(1), "20210725");
         assertNotNull(response);
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
-
+    }
+    @Test
+    public void getTaskById(){
+        lenient().doReturn(task1).when(taskService).getTasktById(any());
+        ResponseEntity<Task> response = taskListController.getTaskById(Long.valueOf(1));
+        assertNotNull(response);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
     }
 
+    @Test
+    public void deleteTask() throws Exception {
+        lenient().doNothing().when(taskService).deleteTask(any());
+        HttpStatus httpStatus = taskListController.deleteTask(Long.valueOf(1));
+        assertNotNull(httpStatus);
+        assertEquals(HttpStatus.OK, httpStatus);
+    }
 }

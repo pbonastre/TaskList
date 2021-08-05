5483dda37137f01936fbf7e6a6fd501c7a0a739d
Paula Bonastre <pbonastre@gmail.com>
2021-08-05 14:48:12 +0200

    Remove RestController

--- src/main/java/com/mimacom/taskList/TaskListApplication.java
+++ src/main/java/com/mimacom/taskList/TaskListApplication.java
@@ -2,12 +2,10 @@ package com.mimacom.taskList;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
-@RestController
 public class TaskListApplication {
 
 	public static void main(String[] args) {
